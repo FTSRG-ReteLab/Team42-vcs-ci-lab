48c495d6c7e8a1208cee6e7fc1ccf0327b2d5500
mmoczar <moczarmarcell@gmail.com>
2019-04-08 06:30:02 -0700

    Timer added to constructor

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,21 +2,28 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
-	Timer timer = new Timer();
+	public Timer timer;
 
+	public TrainControllerImpl()
+	{
+		timer = new Timer();
 
-	timer.scheduleAtFixedRate(new TimerTask() {
-  	@Override
-  		public void run() {
-   			followSpeed();
-  		}
-	}, 2*60*1000, 2*60*1000);
+		timer.scheduleAtFixedRate(new TimerTask() {
+  			@Override
+  			public void run() {
+   				followSpeed();
+  			}
+	}, 1000, 1000);
+	}
+
+	
 
 
 	@Override
